42c7a1807743e4b6dc025e52450b5267a6e26935
Anton.Titov <BenqTco`1>
2025-08-22 14:05:36 +0300

    Коммент про Chargeability

--- src/profit_center_analysis.py
+++ src/profit_center_analysis.py
@@ -26,4 +26,4 @@ df_summary = df_pl_summary.merge(row_with_profit_center, on=['Год', 'Меся
 report_path = REPORT_PATH % f"[{','.join([str(x) for x in years_to_process])}]"
 save_summary_with_format(df_summary, report_path)
 print(report_path)
-
+print(f"Chargeability суммарный за год во вкладке `Chargeability_отчет` файла PL_XX {max(years_to_process)}.xlsx")
